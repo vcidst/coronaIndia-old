a2b2c678325696a9b35769710ff86c8767b0d65f
vcidst <beingshailendra@gmail.com>
2020-03-22 10:59:54 +0530

    decouple logic from server, use fastapi, define api schema, documation available at /docs when run

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import functools
+from relationships import get_nationality, get_travel_place, get_relationship
+from schema import PatientList
+
+app = FastAPI()
+
+@app.post("/")
+def single(patients: PatientList):
+    return process_records(patients)
+
+def process_records(records):
+    return {
+        "patients": [
+            {r.patientId: record_processor(r.notes)} for r in records.patients
+        ]
+    }
+
+@functools.lru_cache(30000)
+def record_processor(sent):
+    return {
+        "nationality": get_nationality(sent),
+        "travel": get_travel_place(sent),
+        "relationship": get_relationship(sent),
+    }
+
